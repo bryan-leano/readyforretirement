e1741b32226865fc2f68cc485956ddcb1392a7cb
Bryan Leanoe <bryan.leano@gmail.com>
2019-10-29 19:34:48 -0600

    added user inputs

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-//  age
-        int age = 29;
-        String name = "Bryan";
+
         int retirementAge = 65;
-        int salary = 100000;
         int retirementMoney = 800000;
 
-        System.out.println("You are " + name + " and you are " + age + " years old.");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
+
+        System.out.println("Hello " + name + "! What is your age?");
+        int age = sc.nextInt();
+
+        System.out.println(name + ", retirement is at age " + retirementAge + ". Let's see if you're on track for retirement.");
+        System.out.println("What is your annual salary?");
+
+        int salary = sc.nextInt();
+
 
         if (retirementMoney >= (salary * 10)) {
-            System.out.println("Congratulations, you are ready for retirement!");
+            System.out.println("Congratulations, you are on track for retirement!");
         } else {
             System.out.println("Oops, you need to save more for retirement!");
         }
